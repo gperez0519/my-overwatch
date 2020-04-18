9f14cdbb9db797f9582778635f133b15a51c15f6
Gabriel Perez <gperez0519@gmail.com>
2020-04-17 22:12:26 -0400

    Able to retrieve ow profile info

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const appName = 'My Overwatch';
 /** OVERWATCH GENERAL RESPONSES **/
 const responses = {
     WELCOME: "Welcome to My Overwatch! We can tell you your stats of your Overwatch progress. Say get my stats to hear your stats of your Overwatch profile.",
+    PLEASE_WAIT: "Please wait while we try to retrieve that profile information",
     BATTLETAG_NUMBER_INQUIRY: "Perfect! Now, please read off the number portion of your battle tag after the hashtag symbol.",
     GOODBYE: "Thank you for choosing My Overwatch! Always thrive to attain Grand Master if you haven't already. Good luck in your battles! Come back again for an updated look at your Overwatch profile and some tips to help in your gameplay to get you to the top!",
-    DEFAULT_ERROR_BATTLETAG: "Sorry, we could not find that battletag. Please repeat the full battle tag. For example, say soldier#1337",
+    DEFAULT_ERROR_BATTLETAG: "Sorry, we could not find that battletag. Please repeat the battle tag username before the hashtag. For example, say illusion or elite",
     DEFAULT_ERROR_PLATFORM: "Sorry, we did not recognize that platform. Please say either Xbox, PC, or Playstation.",
     PLATFORM_INQUIRY: "Great! Which platform do you want to get your stats for? Xbox, PC or Playstation?"
 }
@@ -16,7 +17,6 @@ const responses = {
 /** COOL OVERWATCH SOUND EFFECTS **/
 const heroes = {
     SOLDIER76: "test value",
-
 }
 
 
@@ -53,7 +53,7 @@ const GetMyStatsIntentHandler = {
         return (handlerInput.requestEnvelope.request.type === 'IntentRequest'
             && handlerInput.requestEnvelope.request.intent.name === 'GetMyStatsIntent');
     },
-    handle(handlerInput) {
+    async handle(handlerInput) {
         outputSpeech = responses.DEFAULT_ERROR_BATTLETAG;
         let intent = handlerInput.requestEnvelope.request.intent;
 
@@ -65,17 +65,26 @@ const GetMyStatsIntentHandler = {
 
         console.log("Captured Battletag username: " + battletag_username);
         console.log("Captured Battletag number: " + battletag_number);
-        console.log("Captured Platform " + platform);
+        console.log("Captured Platform: " + platformVal);
+
+        try {
+
+            await callDirectiveService(handlerInput);
+        } catch (err) {
+
+            // if it failed we can continue, just the user will wait longer for first response
+            console.log("There was an issue attempting to send the progressive response while searching for overwatch profile of the given battletag " + err);
+        }
 
         // SET THE PLATFORM TO THE OVERWATCH STATS API PARAMETER TYPE EITHER pc, xbl or psn
         if (platformVal.toLowerCase() == "xbox") {
             platform = "xbl";
-        } else if (platformVal.toLowerCase() == "pc") {
+        } else if (platformVal.toLowerCase() == "pc" || platformVal.toLowerCase() == "peesee" || platformVal.toLowerCase() == "pz" || platformVal.toLowerCase() == "peezee") {
             platform = "pc";
         } else if (platformVal.toLowerCase() == "playstation") {
             platform = "psn";
         }
-
+        
         // CALL THE OVERWATCH STATS API TO GET THE PROFILE INFORMATION FOR THE PASSED BATTLETAG WITH PLATFORM
         if (battletag_username && battletag_number && platform) {
 
@@ -85,18 +94,21 @@ const GetMyStatsIntentHandler = {
             // CONCAT USERNAME DASH AND NUMBER TO GET THE STATS
             battletag = battletag_username + "-" + battletag_number;
 
-           (async () => {
-                    const stats = await ow.getAllStats(battletag, platform);
-                    console.log(JSON.stringify(stats));
-                    profileInfoRetrieved = true;
-                })();
+            console.log("Full translated battletag: " + battletag);
+            console.log("Platform recognized: " + platform);
+
+            const stats = await ow.getAllStats(battletag, platform);
+            console.log(JSON.stringify(stats));
+            profileInfoRetrieved = true;
+
+            if (profileInfoRetrieved) {
+                outputSpeech = `Profile info retrieved successfully! Hello ${battletag_username}! You are currently ranked ${stats.rank.damage.sr > 2000 ? 'gold!' : 'not gold yet'}`;
+            } else {
+                outputSpeech = `Profile info retrieval failed! We heard you say ${platformVal} as the platform. ${battletag_username} as the username of the battletag and ${battletag_number} as the number in the battletag.`;
+            }
             
         } 
-        if (profileInfoRetrieved) {
-            outputSpeech = `Profile info retrieved successfully! We heard you say ${platformVal} as the platform. ${battletag_username} as the username of the battletag and ${battletag_number} as the number in the battletag.`;
-        } else {
-            outputSpeech = `Profile info retrieval failed! We heard you say ${platformVal} as the platform. ${battletag_username} as the username of the battletag and ${battletag_number} as the number in the battletag.`;
-        }
+        
         
         return handlerInput.responseBuilder
             .speak(outputSpeech)
@@ -106,6 +118,30 @@ const GetMyStatsIntentHandler = {
 
     },
 };
+
+function callDirectiveService(handlerInput) {
+    // Call Alexa Directive Service.
+    const requestEnvelope = handlerInput.requestEnvelope;
+    const directiveServiceClient = handlerInput.serviceClientFactory.getDirectiveServiceClient();
+  
+    const requestId = requestEnvelope.request.requestId;
+    const endpoint = requestEnvelope.context.System.apiEndpoint;
+    const token = requestEnvelope.context.System.apiAccessToken;
+  
+    // build the progressive response directive
+    const directive = {
+      header: {
+        requestId,
+      },
+      directive: {
+        type: 'VoicePlayer.Speak',
+        speech: `${responses.PLEASE_WAIT}`,
+      },
+    };
+  
+    // send directive
+    return directiveServiceClient.enqueue(directive, endpoint, token);
+  }
 
 
 const HelpIntentHandler = {
@@ -205,4 +241,5 @@ exports.handler = skillBuilder
         SessionEndedRequestHandler
     )
     .addErrorHandlers(ErrorHandler)
+    .withApiClient(new Alexa.DefaultApiClient())
     .lambda();
