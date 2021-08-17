ed87f73352f0e1aae4c3da2be44a264279f57d09
Gabriel Perez <gperez0519@gmail.com>
2021-08-16 20:46:06 -0400

    Revamped interaction for adventage experience.

--- index.js
+++ index.js
@@ -7,19 +7,26 @@ const appName = 'My Overwatch';
 
 /** OVERWATCH GENERAL RESPONSES **/
 const responses = {
-    WELCOME: "Welcome to My Overwatch! We can tell you your stats of your Overwatch progress. Say get my stats to hear your stats of your Overwatch profile.",
+    GREETING: "Welcome to My Overwatch! We can tell you your stats of your Overwatch progress. Say get my stats to hear your stats of your Overwatch profile.",
+    GREETING_PERSONALIZED: "Welcome Illusion! Please. have a seat and take a load off! Here's a round on the house!",
+    YOU_ARE_WELCOME: "Not a problem at all buddy!",
+    RANK_PERSONALIZED_BEGIN: "Hmm. I see that you are working hard on your rank",
+    GREETING_RESPONSE: "I'm doing just well. now that you are here!",
     NEED_TO_LINK_MESSAGE: 'Before we can continue, you will need to link your account to the My Overwatch skill using the card that I have sent to the Alexa app.',
     TOP_MENU: "Great! We can tell you your stats of your Overwatch progress. Say get my stats to hear your stats of your Overwatch profile.",
     OVERWATCH_SERVICE_UNAVAILABLE: "Oh no! The My Overwatch service is not available at the moment. Please try again later.",
     PLEASE_WAIT: "Please wait while we try to retrieve that profile information",
     PLACEMENTS_NOT_COMPLETE: "You have not placed yet in the Competitive season. Make sure you do so in order to hear about your ranking info.",
     BATTLETAG_NUMBER_INQUIRY: "Perfect! Now, please read off the number portion of your battle tag after the hashtag symbol.",
-    GOODBYE: "Thank you for choosing My Overwatch! Always thrive to attain Grand Master if you haven't already. Good luck in your battles! Come back again for an updated look at your Overwatch profile and some tips to help in your gameplay to get you to the top!",
+    GOODBYE: "You are always welcome here my friend! Stop by next time and we can catch up again. Good luck in your battles!",
     DEFAULT_ERROR_BATTLETAG: "Sorry, we could not find that battletag. Please repeat the battle tag username before the hashtag. For example, say illusion or elite",
     DEFAULT_ERROR_PLATFORM: "Sorry, we did not recognize that platform. Please say either Xbox, PC, or Playstation.",
     PLATFORM_INQUIRY: "Great! Which platform do you want to get your stats for? Xbox, PC or Playstation?",
     PLEASE_REPEAT: "Sorry, we did not hear from you.",
-    ANYTHING_ELSE: "Is there anything else that you would like to know?"
+    ANYTHING_ELSE: "Is there anything else that you would like to know?",
+    POUR_DRINK_AUDIO: "<audio src='soundbank://soundlibrary/household/water/pour_water_01'/>",
+    DOOR_OPEN_AUDIO: "<audio src='soundbank://soundlibrary/home/amzn_sfx_door_open_01'/>",
+    GLASS_CLINK_AUDIO: "<audio src='soundbank://soundlibrary/glass/clink/glasses_clink_04'/>"
 }
 
 /** CUSTOM FUNCTIONS **/
@@ -66,7 +73,7 @@ const CheckAccountLinkedHandler = {
     handle(handlerInput) {
         const speakOutput = responses.NEED_TO_LINK_MESSAGE;
         return handlerInput.responseBuilder
-        .speak(speakOutput)
+        .speak("<voice name='Brian'>" + speakOutput + "</voice>")
         .withLinkAccountCard()
         .getResponse();
     },
@@ -80,18 +87,43 @@ const LaunchRequestHandler = {
     handle(handlerInput) {
 
         // welcome message
-        let welcomeText = responses.WELCOME;
+        let welcomeText = responses.DOOR_OPEN_AUDIO + responses.GREETING_PERSONALIZED + responses.POUR_DRINK_AUDIO + responses.GLASS_CLINK_AUDIO;
+
+        let rePromptText = "You know it is quite rude to not thank someone for giving you a round on the house you know.";
 
         // welcome screen message
-        let displayText = responses.WELCOME;
+        let displayText = responses.GREETING_PERSONALIZED;
 
         return handlerInput.responseBuilder
-            .speak(welcomeText)
-            .reprompt(welcomeText)
+            .speak("<voice name='Brian'>" + welcomeText + "</voice>")
+            .reprompt("<voice name='Brian'>" + rePromptText + "</voice>")
             .withSimpleCard(appName, displayText)
             .getResponse();
     }
 }; // end of launch request handler
+
+const ThanksIntentHandler = {
+    canHandle(handlerInput) {
+        return (handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'ThanksIntent');
+    },
+    handle(handlerInput) {
+
+        // welcome message
+        let youAreWelcomeResponse = responses.YOU_ARE_WELCOME;
+
+        let rePromptText = "Are you okay my friend?";
+
+        // welcome screen message
+        let displayText = responses.WELCOME_PERSONALIZED;
+
+        return handlerInput.responseBuilder
+            .speak("<voice name='Brian'>" + youAreWelcomeResponse + "</voice>")
+            .reprompt("<voice name='Brian'>" + rePromptText + "</voice>")
+            .withSimpleCard(appName, displayText)
+            .getResponse();
+    }
+}; // end of thanks intent handler
 
 const GetMyStatsIntentHandler = {
     canHandle(handlerInput) {
@@ -100,11 +132,17 @@ const GetMyStatsIntentHandler = {
     },
     async handle(handlerInput) {
         outputSpeech = responses.DEFAULT_ERROR_BATTLETAG;
-        let intent = handlerInput.requestEnvelope.request.intent;
+        //let intent = handlerInput.requestEnvelope.request.intent;
 
-        let platformVal = intent.slots.platform.value;
-        let battletag_username = intent.slots.battletag_username.value;
-        let battletag_number = intent.slots.battletag_number.value;
+        //let platformVal = intent.slots.platform.value;
+        //let battletag_username = intent.slots.battletag_username.value;
+        //let battletag_number = intent.slots.battletag_number.value;
+
+        let platformVal = "PC";
+        let battletag_username = "illusion";
+        let battletag_number = "1474";
+
+        let rePromptText = "Well, I guess you don't find that to be very exciting but in any case I will leave you to it. Let me know if you need anything else.";
 
         console.log("Captured Battletag username: " + battletag_username);
         console.log("Captured Battletag number: " + battletag_number);
@@ -112,7 +150,7 @@ const GetMyStatsIntentHandler = {
 
         try {
 
-            await callDirectiveService(handlerInput);
+            //await callDirectiveService(handlerInput);
         } catch (err) {
 
             // if it failed we can continue, just the user will wait longer for first response
@@ -152,8 +190,7 @@ const GetMyStatsIntentHandler = {
                     outputSpeech = responses.OVERWATCH_SERVICE_UNAVAILABLE;
                 } else {
                     // get the rank if user has completed their placements
-                    outputSpeech = `Profile info retrieved successfully! 
-Hello ${battletag_username}! ${!isObjectEmpty(stats.rank) ?
+                    outputSpeech = `${responses.RANK_PERSONALIZED_BEGIN}! ${!isObjectEmpty(stats.rank) ?
                             (stats.rank.damage.sr > 4000
                                 ? 'You are currently ranked Grandmaster!'
                                 : stats.rank.damage.sr < 3999 && stats.rank.damage.sr > 3500
@@ -165,12 +202,12 @@ Hello ${battletag_username}! ${!isObjectEmpty(stats.rank) ?
                                             : stats.rank.damage.sr < 2499 && stats.rank.damage.sr > 2000
                                                 ? 'You are currently ranked Gold!'
                                                 : stats.rank.damage.sr < 1999 && stats.rank.damage.sr > 1500
-                                                    ? 'You are currently ranked Silver!'
-                                                    : 'You are currently ranked Bronze!') : responses.PLACEMENTS_NOT_COMPLETE
+                                                    ? 'You are currently ranked Silver but you got a ways to go!'
+                                                    : 'You are currently ranked Bronze but you got a ways to go!') : responses.PLACEMENTS_NOT_COMPLETE
                         }
-You are level ${stats.level} 
-and prestige level ${stats.prestige}.
-Your most played hero is ${!isObjectEmpty(mostPlayed) ? Object.keys(mostPlayed.quickplay)[0] : ""}`;
+Wow. you really love to play ${!isObjectEmpty(mostPlayed) ? Object.keys(mostPlayed.quickplay)[0] : ""} alot in Quick Play.
+and you seem to enjoy playing alot with ${!isObjectEmpty(mostPlayed) ? Object.keys(mostPlayed.competitive)[0] : ""} in Competitive.
+You must really dominate the field with them. Keep going and you will be the best in no time.`;
                     console.log(JSON.stringify(stats));
                 }
 
@@ -182,8 +219,8 @@ Your most played hero is ${!isObjectEmpty(mostPlayed) ? Object.keys(mostPlayed.q
 
 
         return handlerInput.responseBuilder
-            .speak(`${outputSpeech} ${responses.ANYTHING_ELSE}`)
-            .reprompt(`${responses.PLEASE_REPEAT} ${responses.ANYTHING_ELSE}`)
+            .speak(`<voice name='Brian'>${responses.GREETING_RESPONSE} ${outputSpeech}</voice>`)
+            .reprompt(`<voice name='Brian'>${rePromptText}</voice>`)
             //.withStandardCard(appName, card)
             .getResponse();
 
@@ -199,6 +236,10 @@ function callDirectiveService(handlerInput) {
     const endpoint = requestEnvelope.context.System.apiEndpoint;
     const token = requestEnvelope.context.System.apiAccessToken;
 
+    console.log("Token:",token);
+    console.log("EndPoint:", endpoint);
+    console.log("Request ID:", requestId);
+
     // build the progressive response directive
     const directive = {
         header: {
@@ -206,7 +247,7 @@ function callDirectiveService(handlerInput) {
         },
         directive: {
             type: 'VoicePlayer.Speak',
-            speech: `${responses.PLEASE_WAIT}`,
+            speech: `<voice name='Brian'>${responses.RANK_PERSONALIZED_BEGIN}</voice>`,
         },
     };
 
@@ -239,6 +280,20 @@ const YesIntentHandler = {
 
         return handlerInput.responseBuilder
             .speak(responses.TOP_MENU)
+            .reprompt(`${responses.PLEASE_REPEAT} ${responses.ANYTHING_ELSE}`)
+            .getResponse();
+    },
+};
+
+const NoIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.NoIntent';
+    },
+    handle(handlerInput) {
+
+        return handlerInput.responseBuilder
+            .speak(responses.GOODBYE)
             .reprompt(`${responses.PLEASE_REPEAT} ${responses.ANYTHING_ELSE}`)
             .getResponse();
     },
@@ -305,9 +360,11 @@ exports.handler = skillBuilder
     .addRequestHandlers(
         CheckAccountLinkedHandler,
         LaunchRequestHandler,
+        ThanksIntentHandler,
         GetMyStatsIntentHandler,
         HelpIntentHandler,
         YesIntentHandler,
+        NoIntentHandler,
         CancelAndStopIntentHandler,
         SessionEndedRequestHandler
     )
